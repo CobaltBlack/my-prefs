4ee11cce857e65baafd69432517c440fbcd2628e
CobaltBlack <007.ericliu@gmail.com>
2021-05-22 22:00:13 -0700

    Add setup script and readme

---
+++ README.md
@@ -0,0 +1,1 @@
+Just a place to store my preferences for bash and VS Code. Nothing special.
---
+++ setup.sh
@@ -0,0 +1,5 @@
+cp config/bash/.bash_aliases ~/
+cp config/bash/.bashrc_custom ~/
+cp config/bash/.dircolors ~/
+
+cp config/tmux/.tmux.conf ~/
